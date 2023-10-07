09dd152c4392a96b4e68068a629236de953a036d
zablockil <133679739+zablockil@users.noreply.github.com>
2023-10-07 22:33:15 +0200

    Add files via upload

--- smime/misc/csv_database.sh
+++ smime/misc/csv_database.sh
@@ -17,13 +17,13 @@ path_cert="public_user/user.crt"
 user_alias="user"
 
 
-csv_NotAfter=$(date -d "$(openssl x509 -noout -enddate -in $path_cert | awk -F '=' '{print $NF}')" --rfc-3339=seconds)
-csv_NotBefore=$(date -d "$(openssl x509 -noout -startdate -in $path_cert | awk -F '=' '{print $NF}')" --rfc-3339=seconds)
+csv_NotAfter="$(date -d "$(openssl x509 -noout -enddate -in $path_cert | awk -F '=' '{print $NF}')" --rfc-3339=seconds)"
+csv_NotBefore="$(date -d "$(openssl x509 -noout -startdate -in $path_cert | awk -F '=' '{print $NF}')" --rfc-3339=seconds)"
 csv_Serial="$(openssl x509 -noout -serial -in $path_cert | awk -F '=' '{print $NF}')"
 csv_keyUsage="$(openssl x509 -noout -ext keyUsage -nameopt oneline -in $path_cert | awk '/X509v3 Key Usage/ {getline;print gensub("^ +","","g",$0)}')"
-csv_Subject=$(openssl x509 -noout -subject -nameopt oneline,-esc_2253 -in $path_cert | awk -F 'subject=' '{print $NF}')
+csv_Subject="$(openssl x509 -noout -subject -nameopt oneline,-esc_2253 -in $path_cert | awk -F 'subject=' '{print $NF}')"
 csv_SubjectAlternativeName="$(openssl x509 -noout -ext subjectAltName -nameopt oneline -in $path_cert | awk '/X509v3 Subject Alternative Name/ {getline;print gensub("^ +","","g",$0)}')"
-csv_subjectKeyIdentifier="$(openssl x509 -noout -ext subjectKeyIdentifier -in $path_cert | awk '/X509v3 Subject Key Identifier/ {getline;print gensub("^ +","","g",$0)}')"
+csv_subjectKeyIdentifier="$(openssl x509 -noout -ext subjectKeyIdentifier -in $path_cert | awk 'NR==2 {print $1}')"
 csv_fingerprint_sha256="$(openssl x509 -noout -fingerprint -sha256 -in $path_cert | awk -F '=' '{print $NF}')"
 csv_fingerprint_sha1="$(openssl x509 -noout -fingerprint -sha1 -in $path_cert | awk -F '=' '{print $NF}')"
 
--- smime/misc/standalone_DSA_and_DH_dual-key.sh
+++ smime/misc/standalone_DSA_and_DH_dual-key.sh
@@ -6,7 +6,7 @@
 test_commonName="Glenn Quagmire"
 test_email="quagmire@example.com"
 
-custom_days=$(($(($(date +%s -d "10 years") - $(date +%s)))/$((60*60*24))))
+custom_days="$(($(($(date +%s -d "10 years") - $(date +%s)))/$((60*60*24))))"
 user_usage_period_days="1185"
 root_usage_period_days="$custom_days"
 
@@ -49,7 +49,7 @@ dh_paramgen_subprime_len_user_E="256"
 
 # -set_serial "0x$(custom_serial)"
 custom_serial () {
-	echo $(shuf -i 1-7 -n 1)$(openssl rand -hex 20) | cut -c1-16
+	echo "$(shuf -i 1-7 -n 1)$(openssl rand -hex 20)" | cut -c1-16
 }
 
 # hex RootCA -set_serial "0x00"
@@ -106,7 +106,7 @@ EOF
 
 OPENSSL_CONF="private/root/config_root.cfg"
 
-openssl req -new -x509 -days $root_usage_period_days -$default_md_cert_root -set_serial "0x$(custom_serial)" -config "private/root/config_root.cfg" -key "private/root/key_root.pem" > "private/root/cert_root.crt"
+openssl req -new -x509 -days "$root_usage_period_days" -"$default_md_cert_root" -set_serial "0x$(custom_serial)" -config "$OPENSSL_CONF" -key "private/root/key_root.pem" > "private/root/cert_root.crt"
 {
 	openssl x509 -purpose -text -noout -sha256 -fingerprint -in "private/root/cert_root.crt"
 	openssl x509 -noout -fingerprint -sha1 -in "private/root/cert_root.crt"
@@ -185,12 +185,12 @@ EOF
 
 OPENSSL_CONF="private/$user_alias/config_user.cfg"
 
-openssl req -new -config "private/$user_alias/config_user.cfg" -subj "/" -key "private/$user_alias/key_user_S.pem" > "private/$user_alias/csr_user_S.csr"
+openssl req -new -config "$OPENSSL_CONF" -subj "/" -key "private/$user_alias/key_user_S.pem" > "private/$user_alias/csr_user_S.csr"
 
 openssl req -text -noout -verify -in "private/$user_alias/csr_user_S.csr" > "private/$user_alias/csr_user_S.csr.txt"
 
-openssl x509 -req -days $user_usage_period_days -$default_md_cert_user_S -set_serial "0x$(custom_serial)" -in "private/$user_alias/csr_user_S.csr" -CA "private/root/cert_root.crt" -CAkey "private/root/key_root.pem" -extfile "private/$user_alias/config_user.cfg" -extensions x509_smime_DSA_user_S_ext > "private/$user_alias/cert_user_S.crt"
-openssl x509 -req -days $user_usage_period_days -$default_md_cert_user_E -set_serial "0x$(custom_serial)" -in "private/$user_alias/csr_user_S.csr" -CA "private/root/cert_root.crt" -CAkey "private/root/key_root.pem" -force_pubkey "private/$user_alias/key_user_E_pub.der" -keyform DER -extfile "private/$user_alias/config_user.cfg" -extensions x509_smime_DH_user_E_ext > "private/$user_alias/cert_user_E.crt"
+openssl x509 -req -days "$user_usage_period_days" -"$default_md_cert_user_S" -set_serial "0x$(custom_serial)" -in "private/$user_alias/csr_user_S.csr" -CA "private/root/cert_root.crt" -CAkey "private/root/key_root.pem" -extfile "$OPENSSL_CONF" -extensions x509_smime_DSA_user_S_ext > "private/$user_alias/cert_user_S.crt"
+openssl x509 -req -days "$user_usage_period_days" -"$default_md_cert_user_E" -set_serial "0x$(custom_serial)" -in "private/$user_alias/csr_user_S.csr" -CA "private/root/cert_root.crt" -CAkey "private/root/key_root.pem" -force_pubkey "private/$user_alias/key_user_E_pub.der" -keyform DER -extfile "$OPENSSL_CONF" -extensions x509_smime_DH_user_E_ext > "private/$user_alias/cert_user_E.crt"
 
 {
 	openssl x509 -purpose -text -noout -sha256 -fingerprint -in "private/$user_alias/cert_user_S.crt"
