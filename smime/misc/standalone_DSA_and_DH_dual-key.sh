f9b5b54cfef93916e2c30f2cce8ee289aab03259
zablockil <133679739+zablockil@users.noreply.github.com>
2024-01-12 08:43:38 +0100

    Add files via upload

--- smime/misc/standalone_DSA_and_DH_dual-key.sh
+++ smime/misc/standalone_DSA_and_DH_dual-key.sh
@@ -110,7 +110,7 @@ openssl req -new -x509 -days "$root_usage_period_days" -"$default_md_cert_root" 
 {
 	openssl x509 -purpose -text -noout -fingerprint -sha256 -in "private/root/cert_root.crt"
 	openssl x509 -noout -fingerprint -sha1 -in "private/root/cert_root.crt"
-} > "private/root/cert_root.crt.txt"
+} | awk '{ sub(/[ \t]+$/, ""); print }' > "private/root/cert_root.crt.txt"
 
 # USER/Subscriber
 
@@ -195,11 +195,11 @@ openssl x509 -req -days "$user_usage_period_days" -"$default_md_cert_user_E" -se
 {
 	openssl x509 -purpose -text -noout -fingerprint -sha256 -in "private/$user_alias/cert_user_S.crt"
 	openssl x509 -noout -fingerprint -sha1 -in "private/$user_alias/cert_user_S.crt"
-} > "private/$user_alias/cert_user_S.crt.txt"
+} | awk '{ sub(/[ \t]+$/, ""); print }' > "private/$user_alias/cert_user_S.crt.txt"
 {
 	openssl x509 -purpose -text -noout -fingerprint -sha256 -in "private/$user_alias/cert_user_E.crt"
 	openssl x509 -noout -fingerprint -sha1 -in "private/$user_alias/cert_user_E.crt"
-} > "private/$user_alias/cert_user_E.crt.txt"
+} | awk '{ sub(/[ \t]+$/, ""); print }' > "private/$user_alias/cert_user_E.crt.txt"
 
 openssl crl2pkcs7 -nocrl -certfile "private/root/cert_root.crt" -certfile "private/$user_alias/cert_user_S.crt" -certfile "private/$user_alias/cert_user_E.crt" > "public_$user_alias/credential_public.p7b"
 
