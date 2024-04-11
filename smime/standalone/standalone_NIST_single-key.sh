db882274c3ccfeb02802d5041b476350983dc2c0
zablockil <133679739+zablockil@users.noreply.github.com>
2024-04-11 13:08:06 +0200

    Add files via upload

--- smime/standalone/standalone_NIST_single-key.sh
+++ smime/standalone/standalone_NIST_single-key.sh
@@ -8,10 +8,10 @@ test_email="test@example.com"
 
 custom_days="$(($(($(date +%s -d "10 years") - $(date +%s)))/$((60*60*24))))"
 user_usage_period_days="1185"
-root_usage_period_days="$custom_days"
+root_usage_period_days="${custom_days}"
 
 user_alias="user"
-friendly_name_pkcs12="$test_commonName"
+friendly_name_pkcs12="${test_commonName}"
 
 # sha256/sha384/sha512
 default_md_root="sha512"
@@ -31,16 +31,16 @@ custom_serial () {
 #      or
 #        -set_serial "0x$(custom_serial)"
 
-mkdir "public_$user_alias"
+mkdir "public_${user_alias}"
 mkdir "private"
-mkdir "private/$user_alias"
+mkdir "private/${user_alias}"
 
 # ROOT/Issuer
 
 mkdir "private/root"
 openssl version -a > "private/root/openssl_version.txt"
 
-openssl genpkey -algorithm EC -pkeyopt ec_paramgen_curve:$ec_paramgen_curve_root > "private/root/key_root.pem"
+openssl genpkey -algorithm EC -pkeyopt ec_paramgen_curve:${ec_paramgen_curve_root} > "private/root/key_root.pem"
 openssl pkey -text -noout -in "private/root/key_root.pem" > "private/root/key_root.pem.txt"
 openssl pkey -pubout -outform DER -in "private/root/key_root.pem" -out "private/root/key_root_pub.der"
 
@@ -59,7 +59,7 @@ cat <<- EOF > "private/root/config_root.cfg"
 
 [ smime_root_dn ]
 
-	commonName=💌 $test_commonName
+	commonName=💌 ${test_commonName}
 
 [ x509_smime_root_ext ]
 
@@ -70,7 +70,7 @@ cat <<- EOF > "private/root/config_root.cfg"
 	#authorityKeyIdentifier = keyid:always
 	#subjectKeyIdentifier = hash
 		# ↖ standard rfc-sha1
-	subjectKeyIdentifier = "$root_PublicKey_shake256xof32"
+	subjectKeyIdentifier = ${root_PublicKey_shake256xof32}
 	#nsComment = ""
 
 ### END SMIME standalone single-key [NIST EC] ROOT x509v3_config
@@ -78,7 +78,7 @@ EOF
 
 OPENSSL_CONF="private/root/config_root.cfg"
 
-openssl req -new -x509 -days "$root_usage_period_days" -"$default_md_root" -set_serial "0x$(custom_serial)" -config "$OPENSSL_CONF" -key "private/root/key_root.pem" > "private/root/cert_root.crt"
+openssl req -new -x509 -days "${root_usage_period_days}" -"${default_md_root}" -set_serial "0x$(custom_serial)" -config "${OPENSSL_CONF}" -key "private/root/key_root.pem" > "private/root/cert_root.crt"
 {
 	openssl x509 -purpose -text -noout -fingerprint -sha256 -in "private/root/cert_root.crt"
 	openssl x509 -noout -fingerprint -sha1 -in "private/root/cert_root.crt"
@@ -86,13 +86,13 @@ openssl req -new -x509 -days "$root_usage_period_days" -"$default_md_root" -set_
 
 # USER/Subscriber
 
-openssl genpkey -algorithm EC -pkeyopt ec_paramgen_curve:$ec_paramgen_curve_user > "private/$user_alias/key_user.pem"
-openssl pkey -text -noout -in "private/$user_alias/key_user.pem" > "private/$user_alias/key_user.pem.txt"
-openssl pkey -pubout -outform DER -in "private/$user_alias/key_user.pem" -out "private/$user_alias/key_user_pub.der"
+openssl genpkey -algorithm EC -pkeyopt ec_paramgen_curve:${ec_paramgen_curve_user} > "private/${user_alias}/key_user.pem"
+openssl pkey -text -noout -in "private/${user_alias}/key_user.pem" > "private/${user_alias}/key_user.pem.txt"
+openssl pkey -pubout -outform DER -in "private/${user_alias}/key_user.pem" -out "private/${user_alias}/key_user_pub.der"
 
-user_PublicKey_shake256xof32=$(openssl dgst -shake256 "private/$user_alias/key_user_pub.der" | awk -F '=[[:blank:]]' '{print $NF}')
+user_PublicKey_shake256xof32=$(openssl dgst -shake256 "private/${user_alias}/key_user_pub.der" | awk -F '=[[:blank:]]' '{print $NF}')
 
-cat <<- EOF > "private/$user_alias/config_user.cfg"
+cat <<- EOF > "private/${user_alias}/config_user.cfg"
 ### BEGIN SMIME standalone single-key [NIST EC] USER x509v3_config
 
 [ req ]
@@ -108,7 +108,7 @@ cat <<- EOF > "private/$user_alias/config_user.cfg"
 
 [ subject_alt_name ]
 
-	email.0=$test_email
+	email.0=${test_email}
 	#email.1=
 	#otherName.0 =1.3.6.1.5.5.7.8.9;FORMAT:UTF8,UTF8String:
 	#otherName.1 =1.3.6.1.5.5.7.8.9;FORMAT:UTF8,UTF8String:
@@ -121,7 +121,7 @@ cat <<- EOF > "private/$user_alias/config_user.cfg"
 	authorityKeyIdentifier = keyid:always
 	#subjectKeyIdentifier = hash
 		# ↖ standard rfc-sha1
-	subjectKeyIdentifier = "$user_PublicKey_shake256xof32"
+	subjectKeyIdentifier = ${user_PublicKey_shake256xof32}
 	#subjectAltName = @subject_alt_name
 	subjectAltName = critical,@subject_alt_name
 		# ↖ NULL-DN cert
@@ -129,32 +129,32 @@ cat <<- EOF > "private/$user_alias/config_user.cfg"
 ### END SMIME standalone single-key [NIST EC] USER x509v3_config
 EOF
 
-OPENSSL_CONF="private/$user_alias/config_user.cfg"
+OPENSSL_CONF="private/${user_alias}/config_user.cfg"
 
-openssl req -new -config "$OPENSSL_CONF" -subj "/" -key "private/$user_alias/key_user.pem" > "private/$user_alias/csr_user.csr"
-openssl req -text -noout -verify -in "private/$user_alias/csr_user.csr" > "private/$user_alias/csr_user.csr.txt"
+openssl req -new -config "${OPENSSL_CONF}" -subj "/" -key "private/${user_alias}/key_user.pem" > "private/${user_alias}/csr_user.csr"
+openssl req -text -noout -verify -in "private/${user_alias}/csr_user.csr" > "private/${user_alias}/csr_user.csr.txt"
 
-openssl x509 -req -days "$user_usage_period_days" -"$default_md_user" -set_serial "0x$(custom_serial)" -in "private/$user_alias/csr_user.csr" -CA "private/root/cert_root.crt" -CAkey "private/root/key_root.pem" -extfile "$OPENSSL_CONF" -extensions x509_smime_nist_user_ext > "private/$user_alias/cert_user.crt"
-
-{
-	openssl x509 -purpose -text -noout -fingerprint -sha256 -in "private/$user_alias/cert_user.crt"
-	openssl x509 -noout -fingerprint -sha1 -in "private/$user_alias/cert_user.crt"
-} | awk '{ sub(/[ \t]+$/, ""); print }' > "private/$user_alias/cert_user.crt.txt"
-
-openssl crl2pkcs7 -nocrl -certfile "private/root/cert_root.crt" -certfile "private/$user_alias/cert_user.crt" > "public_$user_alias/credential_public.p7b"
-
-openssl x509 -in "private/root/cert_root.crt" > "public_$user_alias/root.crt"
-openssl x509 -in "private/$user_alias/cert_user.crt" > "public_$user_alias/user.crt"
+openssl x509 -req -days "${user_usage_period_days}" -"${default_md_user}" -set_serial "0x$(custom_serial)" -in "private/${user_alias}/csr_user.csr" -CA "private/root/cert_root.crt" -CAkey "private/root/key_root.pem" -extfile "${OPENSSL_CONF}" -extensions x509_smime_nist_user_ext > "private/${user_alias}/cert_user.crt"
 
 {
-	openssl pkey -in "private/$user_alias/key_user.pem"
-	openssl x509 -in "private/$user_alias/cert_user.crt"
+	openssl x509 -purpose -text -noout -fingerprint -sha256 -in "private/${user_alias}/cert_user.crt"
+	openssl x509 -noout -fingerprint -sha1 -in "private/${user_alias}/cert_user.crt"
+} | awk '{ sub(/[ \t]+$/, ""); print }' > "private/${user_alias}/cert_user.crt.txt"
+
+openssl crl2pkcs7 -nocrl -certfile "private/root/cert_root.crt" -certfile "private/${user_alias}/cert_user.crt" > "public_${user_alias}/credential_public.p7b"
+
+openssl x509 -in "private/root/cert_root.crt" > "public_${user_alias}/root.crt"
+openssl x509 -in "private/${user_alias}/cert_user.crt" > "public_${user_alias}/user.crt"
+
+{
+	openssl pkey -in "private/${user_alias}/key_user.pem"
+	openssl x509 -in "private/${user_alias}/cert_user.crt"
 	openssl x509 -in "private/root/cert_root.crt"
-} > "private/$user_alias/credential_private_unencrypted.pem"
+} > "private/${user_alias}/credential_private_unencrypted.pem"
 
-openssl rand -base64 15 > "private/$user_alias/credential_private_password.txt"
+openssl rand -base64 15 > "private/${user_alias}/credential_private_password.txt"
 
-openssl pkcs12 -export -certpbe AES-256-CBC -keypbe AES-256-CBC -macalg sha256 -name "$friendly_name_pkcs12" -in "private/$user_alias/credential_private_unencrypted.pem" -out "private/$user_alias/credential_private_encrypted.p12" -passout file:"private/$user_alias/credential_private_password.txt"
+openssl pkcs12 -export -certpbe AES-256-CBC -keypbe AES-256-CBC -macalg sha256 -name "${friendly_name_pkcs12}" -in "private/${user_alias}/credential_private_unencrypted.pem" -out "private/${user_alias}/credential_private_encrypted.p12" -passout file:"private/${user_alias}/credential_private_password.txt"
 
 #
 # .   standalone_NIST_single-key.sh
